999d29dabf3a20d9737e527a4a32fdf1c7f4d455
Luisa Ojeda <luisaojeda@gmail.com>
2022-05-24 18:19:44 +0200

    done except bonus it

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import listContacts from "./contacts.json";
+import React, { useState } from "react";
 
-function App() {
+//const { contacts } =
+
+export default function App() {
+  const contacts = listContacts.slice(0, 5);
+  const [famous, setFamous] = useState(contacts);
+
+  function randomFamous() {
+    let random = listContacts[Math.floor(Math.random() * listContacts.length)];
+    let exists = false;
+
+    for (let i = 0; i < famous.length; i++) {
+      if (famous.includes(random)) {
+        exists = true;
+      }
+    }
+    if (!exists) {
+      setFamous(famous.concat([random]));
+    }
+  }
+
+
+  const sortName = () => {
+    const arrFamous = [...famous]
+    const sortedFamous = arrFamous.sort((a, b) => {
+      return a.name.localeCompare(b.name)
+    })
+    setFamous(sortedFamous)
+  }
+
+  const sortPopularity = () => {
+    const arrPopularity = [... famous]
+    const sortedPopularity = arrPopularity.sort((a, b) =>{
+      return b.popularity - a.popularity})
+      setFamous(sortedPopularity)
+  }
+
+  const deleteFamous = (id) => {
+    const deletedFamous = famous.filter(a => a.id !== id)
+    setFamous(deletedFamous)
+  }
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <button onClick={randomFamous}>Add Random Contact</button>
+      <button onClick={sortName}>Check them alphabetically</button>
+      <button onClick={sortPopularity}>Check them by popularity</button>
 
-export default App;
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won an Oscar</th>
+            <th>Won an Emmy</th>
+          </tr>
+        </thead>
+        <tbody>
+          {famous.map((contact) => (
+            <tr key={contact._id}>
+              <td>
+                <img
+                  src={contact.pictureUrl}
+                  alt="picture"
+                  width="100px"
+                  height="100px"
+                />
+              </td>
+              <td>
+                <h3>{contact.name}</h3>
+              </td>
+              <td>{contact.popularity.toFixed(2)}</td>
+              <td>{contact.wonOscar && <td>&#127942;</td>}</td>
+              <td>{contact.wonEmmy && <td>&#127942;</td>}</td>
+              <td><button className="delete-btn" onClick={() => deleteFamous(contact.id)}>Delete</button></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
